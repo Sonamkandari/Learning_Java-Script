0e854150812365bc1992eabb4425c9395332bbb5
sonam kandari <sonamkandari2004@gmail.com>
2024-11-16 00:28:22 +0530

    Datatype

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat all js code as newer version
+// alert("hello")
+// alert(3+3) //gaves a error because we are using node.js not browser
+
+let name="sonam"
+let age =18
+let isloggedin=false
+
+//number => 2 to power 53
+//bigint  
+//String=>""
+//boolean=>true/false
+//null =>(it is a stand alone value)(it is the representation of null value)
+//undefined=>when value is not define or assigned
+//symbol =>unique
+
+
+//object 
+
+
+console.log(typeof "sonam kandari")
+console.log(typeof 456)
+console.log(typeof null)//output is it is a object
+console.log(typeof undefined)//output is it is undefined//because it is a type
+
+
+
+

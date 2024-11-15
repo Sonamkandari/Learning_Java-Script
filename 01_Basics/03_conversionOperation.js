0b294fe2839c7475fc786b2d0c7c2703e76bb4db
sonam kandari <sonamkandari2004@gmail.com>
2024-11-16 01:18:44 +0530

    03_coversuion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score ="33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+// type conversion
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);//out put is NaN //not a number
+
+//if the value is null
+let Score =null
+
+console.log(typeof Score);
+console.log(typeof (Score));
+// type conversion
+let InNumber =Number(Score)
+console.log(typeof InNumber);
+console.log(InNumber);//output is 0 (zero)
+
+//"33"=>33 easily get converted
+//if "33abc"=>NAN(not a number)
+//true=>1; false=>0
+
+//type  in boolean 
+let isloggedin = 1 // we can check for "sonam" ,"null",or for empty space also
+let booleanIsloggedIn =Boolean(isloggedin)
+console.log(booleanIsloggedIn);
+

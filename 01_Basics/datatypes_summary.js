22f11c9a07ff38acfd47bafca9b2a7fc6305f867
sonam kandari <sonamkandari2004@gmail.com>
2024-11-17 00:21:41 +0530

    summary

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,16 @@
+//Primitive data type 
+/* 7 types : String , Number , Booolean, null, undefined,symbol,BigInt*/
+
+//Reference type or non premitive data type
+//Aray , Objects, Functions
+
+const score =100
+const scoreValue =10.78
+const outside=null
+let userEmail;//value is undefined
+
+//symbols are useally used in front and liabraries
+
+const id = Symbol('123')
+const anotherID = Symbol('123')
+console.log(id == anotherID);//varification

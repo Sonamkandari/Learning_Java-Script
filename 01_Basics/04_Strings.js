1beeb270b79a3cc778c2c723f8729dfcc265243b
sonam kandari <sonamkandari2004@gmail.com>
2024-11-18 23:20:30 +0530

    string

---
+++ 01_Basics/04_Strings.js
@@ -0,0 +1,18 @@
+const name = "Sonam"
+const surname ="kandari"
+console.log(surname[0]);// it is also working
+console.log(surname.length);
+ //sum = name+surname
+ console.log(name+surname);//String Concatination// it is outdated method
+ //console.log(sum);
+
+ //string interfication
+ console.log('hello my name is ${name} and my surname is ${Kandari}');
+ const gameName = new String('Sonam andari');
+ console.log(gameName[0]);
+ // we can calculate total length of the string
+ console.log(gameName.length);
+ // we can  also convert it into lower case and upper  case
+ console.log(gameName.toUpperCase);//SONAM KANDARI
+ console.log(gameName.charAt(3));
+ 

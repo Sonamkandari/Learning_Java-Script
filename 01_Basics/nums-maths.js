452d557f6b9dcc8bec4f5d39684f8ca113a2da01
sonam kandari <sonamkandari2004@gmail.com>
2024-11-19 10:53:23 +0530

    nums,maths

---
+++ 01_Basics/nums-maths.js
@@ -0,0 +1,10 @@
+const marks = 20
+console.log(marks);// here automatically get to know it is a number
+
+const balance =new Number(200)
+console.log(balance);//but here  when we are getting the out but wr are getting //number:200 type caste
+
+// we are using a *(toString) method
+console.log(balance.toString().length)
+//out put it became a string now with length
+//for decimal values we  used a method to fix

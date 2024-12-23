c39cd63033c56a04cfb98bacf6ae98896bafb5b1
sonam kandari <sonamkandari2004@gmail.com>
2024-12-24 00:37:40 +0530

    lecture 4 of objects in js

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,51 @@
+// declare an object
+//const vscodeUser ={} //this is one a way of declaring an object
+const vscodeUser = new Object()
+vscodeUser.id="10008929"
+vscodeUser.name="Dora"
+vscodeUser.isLoggedIn=false
+
+//console.log(vscodeUser); //this print statement will print all the elements of the object
+
+//Declaring an another object using second way of declaring a method
+const regularUser= {
+   email:"sk@gamil.com",
+   //this will be a object inside a object
+   fullname:{
+     firstname:"sonam",
+     lastname:"kandari"
+
+   }
+}
+console.log(regularUser.fullname)
+
+// merging two or more objects together
+
+const obj1 ={1:"a",2:"b"}
+const obj2 ={3:"a",4:"b"}
+
+//const obj3={obj1,obj2} //important
+
+//const obj3=Object.assign({},obj1,obj2)
+
+//best way for combinnig
+
+const obj3 ={...obj1,...obj2}
+console.log(obj3)
+
+const users =[
+    {
+        id:1,
+        age:23,
+        email:"sk@gmail.com"
+    },
+    //comma separated values
+    {
+        id:1,
+        age:23,
+        email:"sk@gmail.com"
+    }
+]
+users[1].email
+console.log(vscodeUser);
+console.log(Object.keys(vscodeUser));//this will generate all the keys and put it into a array 

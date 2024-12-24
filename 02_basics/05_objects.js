50f862ff1fe8e8d59d4872a10dce1488bddb6b61
sonam kandari <sonamkandari2004@gmail.com>
2024-12-24 12:11:07 +0530

    JSON (Intro)

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,23 @@
+//DESTRUCTURING OF OBJECTD
+
+const course = new Object()
+course.Cousename = "js programming"
+course.price="free"
+course.courseInstructor="Hitesh sir"
+
+console.log(course)
+
+// //basic method(react)
+
+// const navbar = ({company}) =>{
+
+// }
+// navbar(company = "sonam")
+//we get APIs in gthe form of objects and some times in the form of array
+
+//random user api me 
+/*we get some random APIs to use ,we get some random responses to use */
+
+//we also use formatters to understands these responses (APIs sample code) //click on format and beautify
+
+//JSON [java script object Notation]

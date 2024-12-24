ba7ae68a709a3a558e47617f553274dc4d6143a8
sonam kandari <sonamkandari2004@gmail.com>
2024-12-24 14:09:28 +0530

    lecture two of functions

---
+++ 03_Basics/02_functions.js
@@ -0,0 +1,15 @@
+//creating a function
+//function calculateCartprice(num1){//we declare here  a single parameter
+
+/*LEARNING HOW TO PASS REST OPERATOR INSIDE A FUNCTION*/
+function calculateCartprice(VAL1,VAL2,...num1){//*** but if we use ...num now all the arguments can be printed because we use [...NUM[REST OPERATOR [OR]SPREAD]]*/
+    return num1 //return the element
+}
+console.log(calculateCartprice(200,300,455))// but we pass multiple arguements here
+/* but console will print only first argument because in the aboe fuction we passes a single parameter only*/
+
+
+//NOW LEARNING HOW TO PASS AN OBJECT IN A FUNCTION
+
+
+
